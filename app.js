801d1dce8b958b63a36da1e3b6dcfc2269bd9a23
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-04-15 12:35:25 +0300

    small ch

--- app.js
+++ app.js
@@ -17,15 +17,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-//app.use('/users', users);
 
 app.get('/', taskList.showTasks.bind(taskList));
 app.post('/addtask', taskList.addTask.bind(taskList));
