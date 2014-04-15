8918bac0886e8132e0f1c192b37ec3807e28083b
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-04-16 01:40:08 +0300

    small ch

--- routes/tasklist.js
+++ routes/tasklist.js
@@ -44,27 +44,3 @@ TaskList.prototype = {
   }
 }
 
-var mongoose = require('mongoose')
-  , user = require('../models/user.js');
-
-module.exports = UserList;
-
-function UserList(connection) {
-  mongoose.connect(connection);
-}
-
-UserList.prototype = {
-  addUser: function(req,res) {
-    var item = req.body.item;
-    newUser = new user();
-    newUser.Id = new ObjectID();
-    newUser.Name = item.name;
-    newUser.Password = item.password;
-    newUser.save(function savedUser(err){
-      if(err) {
-        throw err;
-      }
-    });
-    res.redirect('/');
-  }
-}
