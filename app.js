752d520e8f5494644e0bcc9e89a54f49934acc88
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-04-15 04:30:33 +0300

    comm all

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+
+var app = express();//.createServer();
+app.configure(function () {
+    app.use(express.logger('dev'));    
+    app.use(express.bodyParser());//it will parse json request bodies (as well as others), and place the result in req.body:
+});
+
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', express.static(__dirname + '/public'));
+
+app.listen(process.env.Port || 3000);
