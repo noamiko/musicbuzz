e90175a56c479ebb2df79f243c9c7eb80d265966
print hello <bacj>
2014-04-17 05:08:50 +0300

    Schemas2

--- routes/userlist.js
+++ routes/userlist.js
@@ -41,7 +41,7 @@ UserList.prototype = {
                     throw err;
                 }
             });
-            res.render('index', {user: item});
+            res.render('index', {user: newUser});
         } else {
             res.render('index', {user: false});
         }
