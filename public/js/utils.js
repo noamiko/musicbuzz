e2edd53b3f61328e02b3db822e0803e128d61eff
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-04-18 04:38:38 +0300

    123

--- public/js/utils.js
+++ public/js/utils.js
@@ -158,7 +158,7 @@ function timer() {
 
 function show_player() {
     alert("showing song :)");
-    content = "<audio controls><source src=" + current_song.url + " type='audio/mpeg'></audio>";
+    content = "<audio autoplay='autoplay' controls><source src=" + current_song.url + " type='audio/mpeg'></audio>";
     $('#' + 'player').html(content);
 
 
