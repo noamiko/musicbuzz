5e6c3666e9524e3122bbddd72d5bdb17c68ceac6
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-04-22 01:44:08 +0300

    update

--- public/js/utils.js
+++ public/js/utils.js
@@ -133,3 +133,19 @@ function refresh_btn() {
         refresh_user();
     }
 }
+
+function hideAddressBar()
+{
+  if(!window.location.hash)
+  {
+      if(document.height < window.outerHeight)
+      {
+          document.body.style.height = (window.outerHeight + 50) + 'px';
+      }
+ 
+      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+}
+ 
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
