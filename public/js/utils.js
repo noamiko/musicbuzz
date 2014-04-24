d580ae2ba058b4e5cc2b4ce087c210b0b1b54605
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-04-24 20:30:36 +0300

    update

--- public/js/utils.js
+++ public/js/utils.js
@@ -74,62 +74,27 @@ function display_song(song, divId) {
 
 
 function display_best_songs(songList, divId) {
+    var content = "";
+    $('#' + divId).html(content);
+
     if (songList !== null && songList !== "") {
-        var content = "";
-        $('#' + divId).html(content);
+        
         for (var i = 0; i < songList.length; i++) {
             content += "<div data-role='controlgroup' data-type='horizontal' class='ui-corner-all ui-controlgroup ui-controlgroup-horizontal'>";
-                    + "<div class='song-row ui-controlgroup-controls'>";
-                    + "<a class='like_btn button ui-btn-check' data-role='button' data-icon='check' onclick='like('" + songList[i]._id + "');'>";
-                    + "<span class='ui-btn-text'></span>";
-                    + "</a>";
-                    + "<a class='song_btn button ui-btn' data-role='button'>";
-                    + "<span class='ui-btn-text'>" + songList[i].title + "</span>";
-                    + "</a>";
-                    + "<a class='dislike_btn button ui-btn-delete' data-role='button' data-icon='delete' onclick='dislike('" + songList[i]._id + "');'>";
-                    + "<span class='ui-btn-text'></span>";
-                    + "</a>";
-                    + "</div>";
-                    + "</div>";
-                    + "</div>";
+            +"<div class='song-row ui-controlgroup-controls'>";
+            +"<a class='like_btn button ui-btn-check' data-role='button' data-icon='check' onclick='like('" + songList[i]._id + "');'>";
+            +"<span class='ui-btn-text'></span>";
+            +"</a>";
+            +"<a class='song_btn button ui-btn' data-role='button'>";
+            +"<span class='ui-btn-text'>" + songList[i].title + "</span>";
+            +"</a>";
+            +"<a class='dislike_btn button ui-btn-delete' data-role='button' data-icon='delete' onclick='dislike('" + songList[i]._id + "');'>";
+            +"<span class='ui-btn-text'></span>";
+            +"</a>";
+            +"</div>";
+            +"</div>";
         }
         $('#' + divId).html(content);
-
-
-//        for (var i = 0; i < songList.length; i++) {
-//            $("#" + divId).append("<div data-role='controlgroup' data-type='horizontal' class='ui-corner-all ui-controlgroup ui-controlgroup-horizontal'><div class='ui-controlgroup-controls'>"
-//                    + "<div id=" + divId + "-row-" + i + " class='song-row ui-controlgroup-controls'></div></div>");
-//            container = document.getElementById(divId + "-row-" + i);
-//            //the vote like button
-//            songList[i].dome = document.createElement("a");
-//            songList[i].dome.setAttribute("class", "like_btn button ui-btn-check");
-//            songList[i].dome.setAttribute("data-role", "button");
-//            songList[i].dome.setAttribute("data-icon", "check");
-//            songList[i].dome.setAttribute("onclick", "like('" + songList[i]._id + "');");
-//            songList[i].dome.innerHTML = "<span class='ui-btn-text'></span>";
-////            songList[i].dome.innerHTML = "<h3>Like</h3>";
-//            container.appendChild(songList[i].dome);
-//
-//            //The button representing the song
-//            songList[i].dome = document.createElement("a");
-//            songList[i].dome.setAttribute("class", "song_btn button ui-btn");
-//            songList[i].dome.setAttribute("data-role", "button");
-//            songList[i].dome.innerHTML = "<span class='ui-btn-text'>" + songList[i].title + "</span>";
-//
-////            songList[i].dome.innerHTML = "<h3>" + songList[i].title + "</h3>";
-//            container.appendChild(songList[i].dome);
-//
-//            //the vote dislike button
-//            songList[i].dome = document.createElement("a");
-//            songList[i].dome.setAttribute("class", "dislike_btn button ui-btn-delete");
-//            songList[i].dome.setAttribute("data-role", "button");
-//            songList[i].dome.setAttribute("onclick", "dislike('" + songList[i]._id + "');");
-//            songList[i].dome.setAttribute("data-icon", "delete");
-//            songList[i].dome.innerHTML = "<span class='ui-btn-text'></span>";
-////            songList[i].dome.innerHTML = "<h3>Dislike</h3>";
-//            container.appendChild(songList[i].dome);
-//        }
-
     }
 }
 
