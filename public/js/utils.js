85886b43f4ffd9ccb90ca3fa3a7214ea2741bb23
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-04-25 01:26:18 +0300

    update

--- public/js/utils.js
+++ public/js/utils.js
@@ -83,9 +83,9 @@ function display_best_songs(songList, divId) {
         for (var i = 0; i < songList.length; i++) {
             content += "<div class='row'>\n"
                     + "<div data-inline='true'>\n"
-                    + "<a href='#' class='like_btn ui-shadow ui-btn ui-corner-all ui-btn-icon-left ui-icon-check ui-btn-inline' onclick='like(" + "'" + songList[i]._id + "'" + ");'></a>\n"
+                    + "<a href='#' class='like_btn ui-shadow ui-btn ui-corner-all ui-btn-icon-left ui-icon-check ui-btn-inline' onclick='like(" + songList[i]._id + ");" + "'></a>\n"
                     + "<a href='#' class='song_btn ui-shadow ui-btn ui-btn-inline ui-corner-all'>" + songList[i].title + "</a>\n"
-                    + "<a href='#' class='dislike_btn ui-shadow ui-btn-inline ui-btn ui-corner-all ui-btn-icon-right ui-icon-delete' onclick='dislike(" + "'" + songList[i]._id + "'" + ");'></a>\n"
+                    + "<a href='#' class='dislike_btn ui-shadow ui-btn-inline ui-btn ui-corner-all ui-btn-icon-right ui-icon-delete' onclick='dislike(" + songList[i]._id + ");" + "'></a>\n"
                     + "</div>\n"
                     + "</div>\n";
         }
