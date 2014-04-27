f0187a2d71d5af09591b7e8e8a04e3d08f718ac9
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-04-27 22:53:21 +0300

    update ui

--- public/js/utils.js
+++ public/js/utils.js
@@ -104,7 +104,7 @@ function changePage(from, to) {
     $.mobile.pageContainer.pagecontainer('change', "#" + to, {
         transition: 'flow',
 //        reload: true
-    }, $('#' + to).show());
+    });
     $('#' + from).hide();
 
     if (from !== "search" && to === 'feed') {
