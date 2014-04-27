0975effdb128357d27408369c57452fab253c406
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-04-27 17:24:54 +0300

    update ui

--- public/js/utils.js
+++ public/js/utils.js
@@ -82,7 +82,7 @@ function display_best_songs(songList, divId) {
             content += "<div class='row'>"
                     + "<div data-inline='true'>"
                     + "<a href='#' class='like_btn ui-btn ui-icon-check ui-btn-icon-notext ui-corner-all ui-btn-inline' onclick='like(" + songList[i]._id + ");'></a>"
-                    + "<a href='#' class='song_btn ui-shadow ui-btn ui-btn-inline ui-corner-all' onclick='alert(&quot" + songList[i].title + "&quot);'>" + short_title(songList[i].title) + "</a>"
+                    + "<a href='#' class='song_btn ui-shadow ui-btn ui-btn-inline ui-corner-all' onclick='alert('" + songList[i].title + "&quot);'>" + short_title(songList[i].title) + "</a>"
                     + "<a href='#' class='dislike_btn ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-btn-inline' onclick='dislike(" + songList[i]._id + ");'></a>"
                     + "</div>"
                     + "</div>";
