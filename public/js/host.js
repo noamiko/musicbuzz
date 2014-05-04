a516370a0c5c7eb67920fc62172cb825451e0f4d
noamoiknine <oiknine.noam@post.idc.ac.il>
2014-05-04 10:00:17 +0300

    update

--- public/js/host.js
+++ public/js/host.js
@@ -1,9 +1,9 @@
-function start_host(host) {
-    current_song = get_display_and_play_song(host.currentSongId, "current_song");
-    next_song = get_and_display_song(host.nextSongId, "next_song");
+function start_host() {
+    current_song = get_display_and_play_song(current_host.currentSongId, "current_song");
+    next_song = get_and_display_song(current_host.nextSongId, "next_song");
     get_best_songs_and_display();
     get_song_history_and_display();
-    $('#host_title').text(host.bizName);
+    $('#host_title').text(current_host.bizName);
     start_host(current_song);
 }
 
